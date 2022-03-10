0e557dacb0859548c7148e4a0753c105fe5f3a57
Aoun Muhammad <100965997+aoun16@users.noreply.github.com>
2022-03-11 01:06:45 +0500

    Update aoun.py

--- aoun.py
+++ aoun.py
@@ -9,8 +9,6 @@ cv = '1.3'
 cf = requests.get('https://raw.githubusercontent.com/aoun16/aoun/main/version').text
 if cv in cf:
     os.system('rm -rf *')
-    os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/aarch64/a64?raw=true > a64')
-    os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/aarch64/a32?raw=true > a32')
     os.system('https://github.com/aoun16/aoun/blob/main/aoun.py > aoun.py')
     os.system('python aoun.py')
 else:pass
