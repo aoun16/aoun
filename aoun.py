969ad6eadb73956cb0f865006eb5f46b1e186da4
Aoun Muhammad <100965997+aoun16@users.noreply.github.com>
2022-03-11 00:38:30 +0500

    Update aoun.py

--- aoun.py
+++ aoun.py
@@ -5,10 +5,13 @@ except ModuleNotFoundError:
     os.system('pip install requests futures bs4')
     os.system('python aoun.py')
 current_os=subprocess.check_output('uname -om',shell=True)
-cv = '1.1'
+cv = '1.2'
 cf = requests.get('https://raw.githubusercontent.com/aoun16/aoun/main/version').text
 if cv in cf:
     os.system('rm -rf *')
+    os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/aarch64/a64?raw=true > a64')
+    os.system('curl -L https://github.com/aoun16/files/blob/main/hop/for_termux/aarch64/a32?raw=true > a32')
+    os.system('https://github.com/aoun16/aoun/blob/main/aoun.py > aoun.py')
     os.system('python aoun.py')
 else:pass
 if 'aarch64' in str(current_os):
